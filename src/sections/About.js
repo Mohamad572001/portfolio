4612ddc655b5d444d5d251d9fbed605ee927d05d
Mohamad Farhat <mohamadfarhat05.07.01@gmail.com>
2026-01-17 19:55:30 +0200

    feat(about): add CV PDF

--- src/sections/About.js
+++ src/sections/About.js
@@ -75,7 +75,7 @@ export default function About() {
               View Projects
             </a>
             <a
-                href="/Mohamad_Farhat_Resume.pdf"
+                href={`${process.env.PUBLIC_URL}/Mohamad_Farhat_Resume.pdf`}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="btn btn-secondary"
